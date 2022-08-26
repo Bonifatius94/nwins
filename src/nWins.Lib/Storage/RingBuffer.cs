7c0ff57de50dc60b46010ade7d7ce82683ecb690
Marco Tröster <marco@troester-gmbh.de>
2022-08-26 16:10:46 +0200

    fix ring buffer sampling - YieldShuffle made sampling bound to total buffer size - now, it's only bound to the batch size - also, the rng was non-static which may lead to same rng results

--- src/nWins.Lib/Storage/RingBuffer.cs
+++ src/nWins.Lib/Storage/RingBuffer.cs
@@ -2,37 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public class RingBuffer<T> : System.Collections.Generic.Queue<T>
+public class RingBuffer<T> : Queue<T>
 {
-    public RingBuffer(int size) { this.size = size; }
+    public RingBuffer(int size)
+        => this.size = size;
 
     private int size = 0;
 
     public void AddItem(T item)
     {
-        // if memory is full, forget oldest item
-        if (Count == size) { Dequeue(); }
-
-        // add new item as youngest
+        if (Count == size)
+            Dequeue();
         Enqueue(item);
     }
 
+    private static Random rng = new Random();
+
     public IEnumerable<T> RandBatch(int batchSize)
     {
-        // generate a random index permutation and select items using indices
-        ///var indices = Enumerable.Range(0, this.Count).LinearShuffle().Take(batchSize);
-        var indices = Enumerable.Range(0, this.Count).YieldShuffle().Take(batchSize);
-        //var indices = randomIndices(batchSize);
-        return indices.Select(x => this.ElementAt(x)).ToList();
-    }
-
-    private List<int> randomIndices(int batchSize) {
-        Random random = new Random();
-        List<int> indices = new List<int>();
-        for(int i = 0; i < batchSize; i++){
-            int randomInt = random.Next() % Count;
-            indices.Add(randomInt);
-        }
-        return indices;
+        var uniqueIndices = new HashSet<int>();
+        while (uniqueIndices.Count < batchSize)
+            uniqueIndices.Add(rng.Next() % this.Count);
+        return uniqueIndices.Select(i => this.ElementAt(i));
     }
 }
